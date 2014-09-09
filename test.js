616b481cc20a775dbed8192286ce97b4e08d92ec
pierreolivier <bouteau.pierreolivier@gmail.com>
2014-09-09 11:02:48 +0200

    license

--- test.js
+++ test.js
@@ -1,2 +1,27 @@
 var ipfiltering = require('./index');
 
+// Initialize the ip filtering
+var middleware = ipfiltering.middleware({});
+
+// add some ips
+ipfiltering.failure('127.0.0.1');
+ipfiltering.failure('127.0.0.1');
+ipfiltering.failure('127.0.0.1');
+ipfiltering.failure('127.0.0.1');
+ipfiltering.failure('127.0.0.1');
+ipfiltering.failure('127.0.0.1');
+
+// middleware function args
+var req = {};
+req.headers = {};
+req.connection = {};
+req.connection.remoteAddress = '127.0.0.1';
+var res = {};
+res.end = function (html) {
+    console.log('status code : ' + res.statusCode);
+    console.log(html);
+};
+var next = function() {};
+
+// execute the ip filtering
+middleware(req, res, next);
